653ae3e282e436b783568312457bd5bb27db6ef6
Waibi Andrew Franklin <waibiaf2@gmail.com>
2026-02-02 06:18:04 +0300

    initial commit

---
+++ src/main/java/com/learning/embarkxeccomercespringbootv40/EmbarkxEccomerceSpringbootV40Application.java
@@ -0,0 +1,13 @@
+package com.learning.embarkxeccomercespringbootv40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarkxEccomerceSpringbootV40Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbarkxEccomerceSpringbootV40Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/embarkxeccomercespringbootv40/EmbarkxEccomerceSpringbootV40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.embarkxeccomercespringbootv40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarkxEccomerceSpringbootV40ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
